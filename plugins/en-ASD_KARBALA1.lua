fcbb1e9c1bf00c1d46d2c853cdd0ffe789ecf6aa
zain211 <gchh130@gmail.com>
2016-06-27 13:43:34 +0300

    Update en-ASD_KARBALA1.lua

--- plugins/en-ASD_KARBALA1.lua
+++ plugins/en-ASD_KARBALA1.lua
@@ -83,9 +83,9 @@ return [[  ❣ setadmin : لرفع اداري للمجموعه
 
 
  Version :1.0
-#Dev : @SAJJADNOORI
-#Dev_bot : @S94_BOT
-#Dev_Channel : @S94IQ
+#Dev : @AILXXZ
+#Dev_bot : @zainty
+#Dev_Channel : @zaintyh
 ]]
 end
 
